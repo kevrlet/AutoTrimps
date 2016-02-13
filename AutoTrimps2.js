3a259da306ae7ce5a73e9d5cf36e49d4bb58dcbe
kevrlet <devin.karolina@gmail.com>
2016-02-13 01:08:01 -0600

    Update AutoTrimps2.js
    Fix to prevent crashing when removing ShieldBlock because upgrade buying is super aggressive. Also auto-disable RemoveShieldBlock when Shield Block is not learned

--- AutoTrimps2.js
+++ AutoTrimps2.js
@@ -531,11 +531,15 @@ function initializeAutoTrimps() {
 
 function removeShieldblock() {
 	if (game.upgrades.Shieldblock.done == 1) {
+		settingChanged('BuyShieldblock');
 		prestigeEquipment("Shield", false, true);
 		game.equipment.Shield.blockNow = false;
 		game.equipment.Shield.tooltip = "A big, wooden shield. Adds $healthCalculated$ health to each soldier per level.";
 		levelEquipment("Shield", 1);
 		game.upgrades.Shieldblock.done = 0;	
+	}
+	else {
+		settingChanged('RemoveShieldblock');
 	}
 }
 
