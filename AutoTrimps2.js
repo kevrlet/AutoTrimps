8ce938928a615dc9402272d7154de7cee99e7b38
kevrlet <devin.karolina@gmail.com>
2016-02-13 00:40:18 -0600

    Update AutoTrimps2.js

--- AutoTrimps2.js
+++ AutoTrimps2.js
@@ -525,8 +525,8 @@ function getBreedTime(remaining) {
 function initializeAutoTrimps() {
     debug('initializeAutoTrimps');
     loadPageVariables();
-    javascript: with(document)(head.appendChild(createElement('script')).src = 'https://rawgit.com/zininzinin/AutoTrimps/master/NewUI.js')._;
-    javascript: with(document)(head.appendChild(createElement('script')).src = 'https://rawgit.com/zininzinin/AutoTrimps/master/Graphs.js')._;
+    javascript: with(document)(head.appendChild(createElement('script')).src = 'https://rawgit.com/kevrlet/AutoTrimps/master/NewUI.js')._;
+    javascript: with(document)(head.appendChild(createElement('script')).src = 'https://rawgit.com/kevrlet/AutoTrimps/master/Graphs.js')._;
 }
 
 function removeShieldblock() {
